5b7a4c9b316039a44eb9515ded033217d0eada44
AbdelkarimYT <abdelkrim.yt98@gmail.com>
2021-09-24 22:13:20 +0100

    init

--- chromebrowser.py
+++ chromebrowser.py
@@ -6,7 +6,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import WebDriverException, NoSuchWindowException, TimeoutException
 from vlc import MediaPlayer
-from time import sleep
 import json
 import random
 
